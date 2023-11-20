c32494a561e22ea228ecad8d16e20e8ef9ba8bfe
Pirisinu <palazzialessandro2@gmail.com>
2023-11-20 15:57:05 +0100

    [ADD]Controller + db connection

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section("content")
+    <h1>home</h1>
+    <div class="container d-flex flex-wrap">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">title</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">original_title</h6>
+              <p class="card-text">nationality</p>
+              <p class="card-text">date</p>
+              <p class="card-text">vote</p>
+              <a href="#" class="card-link">Card link</a>
+            </div>
+          </div>
+    </div>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,19 @@
+@php
+
+@endphp
 <header>
-<h1>Header</h1>
+    <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
 </header>
